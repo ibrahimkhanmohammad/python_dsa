f1d4b8291d9fd76878b87e9d4b205b6ec185c202
ibrahimkhanmohammad <ibrahim.firdous.md@gmail.com>
2026-04-30 21:54:21 +0530

    chanaging content

--- 12_py-conditional_statements/exercise-conditionals/age.py
+++ 12_py-conditional_statements/exercise-conditionals/age.py
@@ -1,12 +1,12 @@
 age = int(input("Enter your age: "))
 
-if 0 < age <= 13:
+if age <= 0:
+    print("Invalid age.")
+elif 0 < age <= 13:
     print("You're child.")
 elif 13 < age <= 19:
     print("You're teen.")
 elif 19 < age <= 59:
     print("You're adult.")
-elif age <= 0:
-    print("Invalid age.")
 else:
     print("You're senior.")
