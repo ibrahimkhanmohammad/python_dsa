07d26dbcf16994d50167f74c5a97cabeb22ed760
ibrahimkhanmohammad <ibrahim.firdous.md@gmail.com>
2026-07-04 08:08:34 +0530

    escseq committing

---
+++ 002_escapseq and operators/01_escseq.py
@@ -0,0 +1,13 @@
+'''
+\n = new line
+\t = tabspace
+\\ = print slash
+\" = print double quotes
+\' = print single quotes
+'''
+
+print("Hello\nIbrahim")
+print("Name:\t Ibrahim")
+print("Ibrahim\\Khan")
+print("Ibrahim\'s Galaxy")
+print("It\'s me \"Ibrahim\"")
