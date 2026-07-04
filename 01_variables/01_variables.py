32da7751011ad0c0c24c337d196a21a1aa953135
ibrahimkhanmohammad <ibrahim.firdous.md@gmail.com>
2026-07-04 05:56:57 +0530

    initial commit

--- 01_variables/01_variables.py
+++
@@ -1,9 +0,0 @@
-# In Python, you just write the name of the variable on the left, followed by the assignment operator, and the value you want to assign the variable on the right. Here's an example of how to declare name and age variables:
-
-# |-----------------------------------------------------------------------------------|
-#                                     VARIABLES
-# |-----------------------------------------------------------------------------------|
-
-# here name holds the value of "Ibrahim Firdous" and Strings are written with single or double quotes, for example the value can be written as either "Ibrahim Firdous" or 'Ibrahim Firdous'.
-name = "Ibrahim Firdous"
-age = 19
