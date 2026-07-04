5f3d951b556fa02bfc7ce95ba40ba3320cd01a52
ibrahimkhanmohammad <ibrahim.firdous.md@gmail.com>
2026-07-04 08:34:16 +0530

    opeartors in py committing

---
+++ 002_escapseq and operators/02_arithmatic_operators.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1%num2)
+print(num1**num2)
---
+++ 002_escapseq and operators/04_logical_operators.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+
+print(num1>50 and num2<25)
+print(num1>50 or num2<25)
+print(not(num1>50 and num2<25))
