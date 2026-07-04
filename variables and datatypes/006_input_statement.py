90a30a4e6a61765f3a9a97c14d5376c764212d9e
ibrahimkhanmohammad <ibrahim.firdous.md@gmail.com>
2026-07-04 07:32:18 +0530

    input statement

---
+++ variables and datatypes/006_input_statement.py
@@ -0,0 +1,5 @@
+name=input("Enter your  name: ")
+age=int(input("Enter your age: "))
+gpa=float(input("Enter your gpa: "))
+
+print(f"Hello, {name} you are {age} years old, and you've got {gpa} gpa.")
