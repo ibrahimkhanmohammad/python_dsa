7cd9ac0a6133caf224da525d1d7c3e1d2ef6482a
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-07-22 18:14:53 +0530

    default arguments...

---
+++ 010_default args/00_basic_idea.py
@@ -0,0 +1,8 @@
+def marks(c, cpp, python, java=0):
+    print(f'c marks: {c}')
+    print(f'cpp marks: {cpp}')
+    print(f'python marks: {python}')
+    print(f'java marks: {java}')
+
+marks(96, 99, 100, 86)
+marks(96, 100, 99)
