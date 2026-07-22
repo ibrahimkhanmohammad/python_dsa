a29135f9463433050956836a1a01361ed0469162
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-07-22 18:15:13 +0530

    keyword or positional arguments...

---
+++ 011_keyword or positional args/00_basic_idea.py
@@ -0,0 +1,8 @@
+def marks(c, cpp, python, java=36):
+    print(f'c marks: {c}')
+    print(f'cpp marks: {cpp}')
+    print(f'python marks: {python}')
+    print(f'java marks: {java}')
+
+marks(cpp=84, python=99, java=84, c= 89)
+marks(python=98, c=89, cpp=86)
