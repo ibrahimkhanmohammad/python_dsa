45c95024d70383e5c152328902541e82c53f14d6
ibrahimkhanmohammad <ibrahimfirdousmd@gmail.com>
2026-07-23 06:44:07 +0530

    introduction to lambda functions

---
+++ 013_lambda functions/1_basic_code.py
@@ -0,0 +1,10 @@
+# def square(num):
+#     return num**2
+
+
+# use lambda function where you can write the code in one step or line.
+square = lambda num: num**2
+
+print(square(5))
+print(square(7))
+
